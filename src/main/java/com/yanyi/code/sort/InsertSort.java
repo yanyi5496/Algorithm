3bc5c99c9a0bbb1e8ae8200c312ea53edca7836f
chenqiang <yanyi5496@gmail.com>
2020-09-22 17:56:44 +0800

    插入排序

--- src/main/java/com/yanyi/code/sort/InsertSort.java
+++ src/main/java/com/yanyi/code/sort/InsertSort.java
@@ -1,14 +1,62 @@
 package com.yanyi.code.sort;
 
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 插入排序
+ * <p>
+ * 一般来说，插入排序都采用in-place在数组上实现。具体算法描述如下：
+ * <p>
+ * 插入排序第一次会使第一位有序
+ * <p>
+ * 第二次会使前两位有序，依此类推
+ * <p>
+ * 遍历未排序的元素，将其和该元素前一个值比较
+ * <p>
+ * 若该元素和前值的大小关系和有序部分不一致，
+ * <p>
+ * 则交换该元素和前值，然后该元素和新的前值继续比较，交换
+ * <p>
+ * 直到找到自己的位置，使前n位依然有序
+ *
  * @author chenqiang
  * @date 2020-09-21
  */
 public class InsertSort {
+    /**
+     * 结果从小到大排列
+     *
+     * @param array
+     */
     private static void sort(int[] array) {
+        int current;
         for (int i = 1; i < array.length; i++) {
-
+            //当前值
+            current = array[i];
+            //索引 初始化为当前值的前一位
+            int preIndex = i - 1;
+            //索引大于0 并且 前一位大于当前值 则当前值需要往更前方移动 重复比较 直到该值位于正确的位置
+            while (preIndex >= 0 && array[preIndex] > current) {
+                //将当前值和前一位交换
+                array[preIndex + 1] = array[preIndex];
+                array[preIndex] = current;
+                //索引前移
+                preIndex--;
+            }
         }
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(array));
     }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 5, 12, 8, 4, 3, 14, 16, 7, 21, 22, 25, 24, 23, 2};
+        StopWatch watch = new StopWatch();
+        watch.start();
+        sort(arr);
+        watch.stop();
+        System.out.println("watch. = " + watch.getTime(TimeUnit.MICROSECONDS));
+    }
+
 }
--- src/main/java/com/yanyi/code/sort/SelectSort.java
+++ src/main/java/com/yanyi/code/sort/SelectSort.java
@@ -1,21 +1,30 @@
 package com.yanyi.code.sort;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 选择排序实现
  * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
  * 然后，再从剩余未排序元素中继续寻找最小（大）元素，
  * 然后放到已排序序列的末尾。以此类推，直到所有元素均排序完毕。
+ * <p>
+ * 第一遍会将最大（小）值选择到起始位置 依此类推
+ *
+ * 在遍历完一次之后才进行元素的交换
  * @author chenqiang
  * @date 2020-09-21
  */
 public class SelectSort {
     private static void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            //最小值的索引 初始为开始循环值
+            //最小值的索引 初始为起始循环值
             int index = i;
+            //前i个已经有序
             for (int j = i; j < array.length; j++) {
+                //遍历每一个值来与当前的最小值比较
                 if (array[j] < array[index]) {
                     //寻找到更小值，更改最小值索引
                     index = j;
@@ -30,7 +39,11 @@ public class SelectSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 5, 12, 8, 4, 3, 14};
+        int[] arr = new int[]{1, 5, 12, 8, 4, 3, 14, 16, 7, 21, 22, 25, 24, 23};
+        StopWatch watch = new StopWatch();
+        watch.start();
         sort(arr);
+        watch.stop();
+        System.out.println("watch. = " + watch.getTime(TimeUnit.MICROSECONDS));
     }
 }
